922964efbd88efc12bd281292a5610f332ddbfad
Rakshitha-2524 <167950970+Rakshitha-2524@users.noreply.github.com>
2024-04-28 19:33:30 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,12 @@
+condition = True
+while(condition):
+    n=int(input())
+    if(n==25):
+        print("congratulations you guessed it right")
+        condition=False
+    elif(n> 25):
+        print("number is too large")
+    elif(n<25 and n>0):
+        print("number is too small")
+    elif(n==0 or n<=0):
+        print('the user want to quits')
