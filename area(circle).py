bd2203c223b6619cde7ade56b920729ef29e3d2f
Rakshitha-2524 <167950970+Rakshitha-2524@users.noreply.github.com>
2024-05-03 21:26:39 +0530

    Add files via upload

---
+++ area(circle).py
@@ -0,0 +1,6 @@
+def cal_area(radius=2,pi=3.14):
+    area=pi*radius**2
+    print("radius=",radius)
+    print("area=",area)
+cal_area(radius=4)
+cal_area()
---
+++ arthimatic.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+
+sum = a + b
+print("sum = ", sum)
+sub = a - b
+print("sub = ", sub)
+mul = a*b
+print("mul =", mul)
